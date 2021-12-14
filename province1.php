71ed4ed1e18e68782b31a9293a6d20fb68819b31
aspnjj <90676501+aspnjj@users.noreply.github.com>
2021-12-14 09:30:09 +0700

    Update province1.php

--- province1.php
+++ province1.php
@@ -1,3 +1,7 @@
 <php
+     $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces");
+     echo $json;
+     $data = json_decode($json);
      
-     ?>
+     var_dump($data);
+?>
