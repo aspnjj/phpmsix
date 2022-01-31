84e9779b311019b6ca511aeb26d11f7b2fe32ee2
aspnjj <90676501+aspnjj@users.noreply.github.com>
2022-01-31 11:25:17 +0700

    Create style.php

---
+++ style.php
@@ -0,0 +1,27 @@
+<html>
+  <head>
+   <style>
+     .g{
+        background-color: lightblue;
+     }
+     #box1{
+        width:250px;
+        height:250px;
+        margin-bottom: 10px;
+        padding : 20px;
+     }
+     #box2{
+        width:400px;
+        height:250px;
+     }
+   </style>
+  </head>
+  <body>
+    <div class="g" id="box1">
+      เบอร์เกอร์สังโฆสปายบอมบ์
+    </div>
+    <div  class="g" id="box2"> 
+      เบอร์เกอร์สังโฆสปายบอมบ์คอปเตอร์น้องใหม่แจ็กเก็ต
+    </div>
+  </body>
+</html>
